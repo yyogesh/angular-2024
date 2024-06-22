07d1d17690d32f2b8bf5da2c02b4ebb4d7353f2c
yyogesh <yogeshggn@hotmail.com>
2024-06-22 08:49:48 +0530

    Angular folder structure

---
+++ Day 9/my-project/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'account', loadChildren: () => import('./modules/account/account.module').then((m) => m.AccountModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
